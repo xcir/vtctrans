9bb5caef142c728ee5513a278aee12c9e3f2a462
LukasMac <lukas.maciulis@gmail.com>
2014-11-06 08:01:36 +0000

    Change spaces to tabs

--- vtctrans.py
+++ vtctrans.py
@@ -11,8 +11,8 @@ def main():
 		
 	opt = ' '.join(argv[1:])
 	r= vtc.execVarnishTest(opt)
-        if(r[0]['result'] != 'passed'):
-                exit(1)
+	if(r[0]['result'] != 'passed'):
+		exit(1)
 
 class VarnishTest:
 	# regexp
@@ -523,7 +523,7 @@ class VarnishTest:
 			
 			#出力
 			self.printVTC(v)
-                return r
+		return r
 
 	def printVTC(self, r):
 		self.printLine('<')
